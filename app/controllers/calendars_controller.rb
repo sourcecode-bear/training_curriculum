1b4561474debfc8a795eeef31a5289e6fbfeed95
sourcecode-bear <76414122+sourcecode-bear@users.noreply.github.com>
2021-01-13 14:44:27 +0900

    シンボル型に修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -44,7 +44,7 @@ class CalendarsController < ApplicationController
       
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wday[wday_num]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wday[wday_num]}
       @week_days.push(days)
     end
 
